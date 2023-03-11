56aafb64b3a9687e1fe63de601946af4b175e7df
MdTarikulIslam <nayon1820@gmail.com>
2023-03-11 14:08:53 +0600

    responsive and working section added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -34,7 +34,7 @@ export default function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white  sticky top-0">
+    <header className="bg-white  stick">
       <nav className="mx-auto flex max-w-7xl items-center justify-between  p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1  ">
           <a href="#" className="-m-1.5 p-1.5">
@@ -171,14 +171,14 @@ export default function NavBar() {
           </a>
         </div>
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog as="div"  className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel data-aos="fade-right" className="fixed inset-y-0 left-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
+        <Dialog.Panel data-aos='fade-right' className="fixed inset-y-0 left-0 z-10 d-width overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between ">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
-                className="h-8 w-auto"
+                className="h-12 w-auto "
                 src="https://suffixit.com/static/media/Suffix-Logo.73142c9b.png"
                 alt=""
               />
@@ -187,12 +187,13 @@ export default function NavBar() {
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(false)}
+  
             >
-              <span  className="sr-only">Close menu</span>
+              <span className="sr-only">Close menu</span>
               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
+          <div className="mt-20 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 <Disclosure as="div" className="-mx-3">
@@ -266,3 +267,4 @@ export default function NavBar() {
   
   )
 }
+
---
+++ src/components/Work/Work.js
@@ -0,0 +1,88 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import "./Work.css";
+
+const Work = () => {
+  return (
+    <div>
+      <div className="flex2">
+        <div>
+          <img
+            data-aos="zoom-in-right"
+            className="work-img"
+            src="https://suffixit.com/static/media/fieldForce.svg.815b7443.png"
+            alt=""
+          />
+        </div>
+        <div>
+          <p className="why-text">Why Choose Us</p>
+          <p className="work-text">
+            We are working with <br /> 8 years experience
+          </p>
+          <p className="consist">
+            We consist of a great team of professional and highly skilled
+            engineers with excellent <br />
+            technical knowledge and experience for developing quality software.{" "}
+          </p>
+
+          <div className="flex-3">
+            <div className="flex-4">
+              <img
+                className="icons"
+                src="https://suffixit.com/static/media/pie-chart.3c8a2401.png"
+                alt=""
+              />
+              <span className="focus-text">Client Focused</span>
+            </div>
+            <div className="flex-4">
+              <img
+                className="icons"
+                src="https://suffixit.com/static/media/team.461ec09b.png"
+                alt=""
+              />
+              <span className="focus-text">Next Gen Technology Provider</span>
+            </div>
+            <div></div>
+          </div>
+          <div className="flex-5">
+            <div>
+              <div className="flex-6">
+                <FontAwesomeIcon className="icon-color" icon={faCheckCircle} />
+                <span className="span-space">24/7 support service</span>
+              </div>
+              <div className="flex-6">
+                <FontAwesomeIcon className="icon-color" icon={faCheckCircle} />
+                <span className="span-space">On-time Delivery</span>
+              </div>
+              <div className="flex-6">
+                <FontAwesomeIcon className="icon-color" icon={faCheckCircle} />
+                <span className="span-space">Within Budget</span>
+              </div>
+            </div>
+            <div>
+              <div className="flex-6">
+                <FontAwesomeIcon className="icon-color" icon={faCheckCircle} />
+                <span>Creating ideas with passion</span>
+              </div>
+              <div className="flex-6">
+                <FontAwesomeIcon className="icon-color" icon={faCheckCircle} />
+                <span>Assure best quality</span>
+              </div>
+              <div className="flex-6">
+                <FontAwesomeIcon className="icon-color" icon={faCheckCircle} />
+                <span>Solution for revolution</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <button className="learn-btn">Learn More</button>
+            <FontAwesomeIcon className="arrow-icon" icon={faArrowRight} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
